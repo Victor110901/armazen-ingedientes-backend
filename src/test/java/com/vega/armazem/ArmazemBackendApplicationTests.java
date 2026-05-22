5e582303c71868fb2c1d15cda1f97e72aa2aab36
Victor110901 <victor.cs2001.vc@gmail.com>
2026-05-22 09:47:11 -0300

    feat: Criação do projeto Spring Boot com controller health para checar estado da aplicação

---
+++ src/test/java/com/vega/armazem/ArmazemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vega.armazem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmazemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
